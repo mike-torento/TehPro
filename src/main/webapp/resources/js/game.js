ace8a0dba56bc9fe8ba4181f849b0c40ac1a4e31
Dmitrii Lytkin <lytkin1@yandex.ru>
2017-02-20 07:57:16 +0400

    Пиздец8.3134567

--- src/main/webapp/resources/js/game.js
+++ src/main/webapp/resources/js/game.js
@@ -1,26 +1,71 @@
 var REQ={};
 $(document).ready(function() {
     validatePermissions();
-    initSliders();
+
 
 
     gameLifeCycle(10);
-    
+
+
+    $.ajax({
+        url: '/TP-dao/startgame',
+        type: 'POST',
+        async: false,
+        dataType: 'json',
+        data: localStorage.getItem("session_id"),
+        contentType: "application/json",
+        success: function (data) {
+            var bankController = data.bankController;
+            var players = bankController.playersList;
+            var player;
+            for(var i=0;i<players.length;i++){
+                if(players[i].user.login===JSON.parse(localStorage.getItem("user")).login){
+                    player = players[i];
+                    break;
+                }
+            }
+
+            MIN_ESM = bankController.bank.countESM;
+            MAX_ESM = MIN_ESM+12;
+            MAX_ESM_PRICE = player.money/MAX_ESM;
+            MIN_ESM_PRICE = bankController.bank.minPriceForESM;
+
+            MAX_EGP = bankController.bank.countEGP;
+            MIN_EGP = MAX_EGP - 12;
+            MAX_EGP_PRICE = bankController.bank.maxPriceForEGP;
+            MIN_EGP_PRICE = 1;
+
+
+
+
+
+
+            initSliders();
+        }
+    });
+
+
     
 });
 
 var MIN_EGP;
 var MAX_EGP;
 
+var MAX_EGP_PRICE;
+var MIN_EGP_PRICE;
+
 var MIN_ESM;
 var MAX_ESM;
+
+var MAX_ESM_PRICE;
+var MIN_ESM_PRICE;
 
 function initSliders() {
     $("#egp-count-slider").slider({
         range: false,
-        min: 13,
-        max: 120,
-        values: [24],
+        min: MIN_EGP,
+        max: MAX_EGP,
+        values: [MIN_EGP],
         slide: function (event, ui) {
             console.log(ui.values[0]);
             $('#egp-count').html(ui.values[0]);
@@ -30,21 +75,21 @@ function initSliders() {
 
     $("#egp-cost-slider").slider({
         range: false,
-        min: 13,
-        max: 120,
-        values: [24],
+        min: MIN_EGP_PRICE,
+        max: MAX_EGP_PRICE,
+        values: [MIN_EGP_PRICE],
         slide: function (event, ui) {
             console.log(ui.values[0]);
-            $('#egp-cost').html(ui.values[0] + "$");
+            $('#egp-cost').html(ui.values[0]);
             rounds = ui.values[0];
         }
     });
 
     $("#esm-count-slider").slider({
         range: false,
-        min: 13,
-        max: 120,
-        values: [24],
+        min: MIN_ESM,
+        max: MAX_ESM,
+        values: [MIN_ESM],
         slide: function (event, ui) {
             console.log(ui.values[0]);
             $('#esm-count').html(ui.values[0]);
@@ -54,12 +99,12 @@ function initSliders() {
 
     $("#esm-cost-slider").slider({
         range: false,
-        min: 13,
-        max: 120,
-        values: [24],
+        min: MIN_ESM_PRICE,
+        max: MAX_ESM_PRICE,
+        values: [MIN_ESM_PRICE],
         slide: function (event, ui) {
             console.log(ui.values[0]);
-            $('#esm-cost').html(ui.values[0] + "$");
+            $('#esm-cost').html(ui.values[0]);
             rounds = ui.values[0];
         }
     });
